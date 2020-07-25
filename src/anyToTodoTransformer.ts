559e3e05b5b5c920ffe5c322d465d6acc6a7517f
Yuki Kokubo <yuukikokubo.pg@gmail.com>
2020-07-26 04:30:25 +0900

    chore: Add todo comment

--- src/anyToTodoTransformer.ts
+++ src/anyToTodoTransformer.ts
@@ -4,6 +4,11 @@ export function anyToTodoTransFormer(ctx: ts.TransformationContext) {
   return (sourceFile: ts.SourceFile) => {
     function visitor(node: ts.Node): ts.Node {
       if (ts.isTypeNode(node) && node.kind === ts.SyntaxKind.AnyKeyword) {
+        // TODO: 現在Todo<any>をTodo<Todo<any>>にしてしまう
+        // なので、parentがTodoの場合は変換しないようにする
+        // AnyKeywordからparentを参照するのは厳しそうなので、
+        // variable declarationとかから肩を判別する？
+
         return ts.createTypeReferenceNode(ts.createIdentifier("Todo"), [
           ts.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
         ]);
