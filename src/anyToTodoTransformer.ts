f135be68a05d4c0adb30bfcece8db18c7ae08e22
Yuki Kokubo <yuukikokubo.pg@gmail.com>
2020-07-26 03:17:30 +0900

    refactor: format

--- src/anyToTodoTransformer.ts
+++ src/anyToTodoTransformer.ts
@@ -4,15 +4,14 @@ export function anyToTodoTransFormer(ctx: ts.TransformationContext) {
   return (sourceFile: ts.SourceFile) => {
     function visitor(node: ts.Node): ts.Node {
       if (ts.isTypeNode(node) && node.kind === ts.SyntaxKind.AnyKeyword) {
-        return ts.createTypeReferenceNode(
-          ts.createIdentifier("Todo"),
-          [ts.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword)]
-        );
+        return ts.createTypeReferenceNode(ts.createIdentifier("Todo"), [
+          ts.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+        ]);
       }
 
       return ts.visitEachChild(node, visitor, ctx);
     }
 
-    return visitor(sourceFile);
+    return ts.visitEachChild(sourceFile, visitor, ctx);
   };
 }
