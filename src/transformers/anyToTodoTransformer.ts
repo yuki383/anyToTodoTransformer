2902e89c2e563a0ee0ea92f2f3d89c98ad01e685
Yuki Kokubo <yuukikokubo.pg@gmail.com>
2020-08-16 05:43:41 +0900

    feat: Add support for converting variable declarations

--- src/transformers/anyToTodoTransformer.ts
+++ src/transformers/anyToTodoTransformer.ts
@@ -1,19 +1,21 @@
 import * as ts from "typescript";
-import { isAnyType } from "../utils/nodeUtils";
-import { todoifyTypeTransformer } from "./todoifyTypeTransformer";
+import { isAnyType, createTodoType } from "../utils/nodeUtils";
+import {
+  todoifyTypeTransformer,
+  isTodoReference,
+  todoifyTypeNode,
+} from "./todoifyTypeTransformer";
 
 export function anyToTodoTransFormer(ctx: ts.TransformationContext) {
   return (sourceFile: ts.SourceFile) => {
     function visitor(node: ts.Node): ts.Node {
       if (ts.isVariableDeclaration(node) && node.type) {
-        const transformed = ts.transform(node.type, [todoifyTypeTransformer])
-          .transformed;
-        if (transformed.length < 1) return node;
+        const hogehoge = hoge(node.type);
 
         const newVariableDeclaration = ts.updateVariableDeclaration(
           node,
           node.name,
-          transformed[0],
+          hogehoge,
           node.initializer
         );
         return newVariableDeclaration;
@@ -24,4 +26,21 @@ export function anyToTodoTransFormer(ctx: ts.TransformationContext) {
 
     return ts.visitEachChild(sourceFile, visitor, ctx);
   };
+}
+
+function hoge(node: ts.TypeNode) {
+  if (isAnyType(node)) {
+    return createTodoType(node);
+  }
+
+  if (ts.isTypeReferenceNode(node) && isTodoReference(node)) {
+    if (!node.typeArguments) return node;
+    const typeArguments = todoifyTypeNode(node.typeArguments[0]);
+    return createTodoType(typeArguments);
+  }
+
+  const transformed = ts.transform(node, [todoifyTypeTransformer]).transformed;
+  if (transformed.length < 1) return node;
+
+  return transformed[0];
 }
--- src/transformers/todoifyTypeTransformer.ts
+++ src/transformers/todoifyTypeTransformer.ts
@@ -1,46 +1,38 @@
-import ts, * as typescript from "typescript";
+import * as ts from "typescript";
 import { isAnyType, createTodoType } from "../utils/nodeUtils";
 
 export function todoifyTypeTransformer(ctx: ts.TransformationContext) {
   return (typeNode: ts.TypeNode) => {
-    let isTodoTypeReference = false;
-
     function visitor(node: ts.Node): ts.Node {
-      if (isTodoReference(node)) {
-        isTodoTypeReference = true;
-        return ts.visitEachChild(node, visitor, ctx);
+      // nodeがTodo型なら型引数をtodoifyする
+      if (ts.isTypeReferenceNode(node) && isTodoReference(node)) {
+        if (node.typeArguments) {
+          return createTodoType(todoifyTypeNode(node.typeArguments[0]));
+        }
       }
 
       // nodeがany型かつTodo<any>の形式でない場合、Todo型に変換する
       if (ts.isTypeNode(node) && isAnyType(node)) {
-        if (isTodoTypeReference) {
-          isTodoTypeReference = false;
-          return node;
-        }
-
-        isTodoTypeReference = false;
-        return createTodoType(
-          ts.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword)
-        );
+        return createTodoType(node);
       }
 
-      isTodoTypeReference = false;
       return ts.visitEachChild(node, visitor, ctx);
-    }
-
-    if (typeNode.kind === ts.SyntaxKind.AnyKeyword) {
-      return createTodoType(typeNode);
-    }
-
-    if (isTodoReference(typeNode)) {
-      isTodoTypeReference = true;
     }
 
     return ts.visitEachChild(typeNode, visitor, ctx);
   };
 }
 
-function isTodoReference(node: ts.Node) {
+export function todoifyTypeNode(node: ts.TypeNode) {
+  const transformed = ts.transform(node, [todoifyTypeTransformer]).transformed;
+  if (transformed.length < 1) {
+    return node;
+  }
+
+  return transformed[0];
+}
+
+export function isTodoReference(node: ts.TypeNode) {
   return (
     ts.isTypeReferenceNode(node) &&
     ts.isIdentifier(node.typeName) &&
